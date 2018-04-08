7e8cf71adf9aafc763ed8bb4c0b1cda94ace14e6
Diego Felix <dfelix@wkm.com.br>
2018-04-08 03:40:50 -0300

    fixing service provider

---
+++ src/ServerErrorResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Difel\HTTPResponses;
+
+use Illuminate\Http\JsonResponse;
+
+class ServerErrorResponse extends JsonResponse
+{
+    /**
+     * ErrorResponse constructor.
+     * @param \Exception $exception
+     * @param null $data
+     * @param int $status
+     * @param array $headers
+     * @param int $options
+     * @internal param string $message
+     * @internal param $data
+     */
+    public function __construct(\Exception $exception, $message = null, $data = null, $status = 500, $headers = [], $options = 0)
+    {
+        $body = [
+            'message' => isset($message) ? $message : $exception->getMessage(),
+            'data' => $data
+        ];
+
+        parent::__construct($body, $status, $headers, $options);
+    }
+}
